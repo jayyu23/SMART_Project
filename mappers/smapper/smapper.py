Tidying up + Bug fixes. Also added in the log weighted sum for score calculation

mappers/smapper/smapper.py
@@ -22,20 +22,28 @@ def score_firmware(energy, area, cycle):
     :param cycle: Cycle data, in cycles
     :return:
     """
-    return -1 * energy * area * cycle
+    return -1 * (math.log10(energy) + math.log10(area) + 1.5 * math.log10(cycle))
 
 
 class Smapper:
-
+    """
+    Smapper, aka. SMART Mapper, is the SMART system's firmware searcher/mapper module. Given a particular hardware
+    architecture, Smapper will coordinate Solver find tiling possibilities, then Operationalizer to create
+    SMART Operations, then call the SMART Estimator and evaluate cost/score using the static score_firmware() method.
+    The user can decide which algorithm to use to conduct the search. Currently supports linear-exhaustive search
+    (keyword: "linear", recommended for users with large compute for a precise/definite answer) and a
+    Bayesian-Optimization based search (keyword "bayes", recommended for users with smaller compute and limited time)
+    """
     def __init__(self):
         self.architecture = None
         self.nn_list = None
         self.nn = None
         self.param_cost_map = OrderedDict()
         self.param_op_map = OrderedDict()
-        self.top_solutions = ()
+        self.top_solutions = ()  # TODO: Legacy code
         self.fw_param_labels = None
-        self.bayes_model = None
+        self.algorithm_map = {"bayes": self.__bayesian_optimization_search,
+                              "linear": self.__linear_search}
 
     def set_architecture(self, arch_path, components_folder, database_table):
         """
@@ -67,72 +75,78 @@ class Smapper:
         self.param_op_map = op.param_operations_map
         self.fw_param_labels = solver.param_labels
 
-    def search_firmware(self, algorithm="linear", print_on=False):
-        if algorithm == "bayes":
-            b_start = time.time()
-            # Conduct Bayesian optimization over the firmware possibilities
-            # Set the parameter boundaries
-            param_bounds = OrderedDict()
-            fw_param_point_set = self.param_op_map.keys()
-            for i in range(len(self.fw_param_labels)):
-                dimension_i = [p[i] for p in fw_param_point_set]
-                # Heuristic: generally large tiles are more efficient
-                max_i, min_i = max(dimension_i) * 1.25, min(dimension_i) * 0.9
-                param_bounds[self.fw_param_labels[i]] = (min_i, max_i)
-            # Now apply the Bayesian model
-            seed_num = math.ceil(len(self.param_op_map) * 0.01)
-            self.bayes_model = BayesianOptimization(f=self.__bayesian_trial,
-                                                    pbounds=param_bounds,
-                                                    random_state=10,
-                                                    verbose=True)
-            self.bayes_model.maximize(seed_num*3, seed_num, kappa=1)
-            bayes_score = abs(self.bayes_model.max['target'])
-            bayes_p = self.__make_discrete_param(self.bayes_model.max['params'])
-            bayes_sol = {self.fw_param_labels[i]: bayes_p[i] for i in range(len(bayes_p))}
-            e = Estimator(self.architecture, self.param_op_map[bayes_p])
-            bayes_eac = e.estimate(['energy', 'area', 'cycle'], analysis=False)
-            print("Bayes Firmware Estimate:", bayes_sol, "Score of:", bayes_score)
-            print("Bayesian Time:", time.time() - b_start)
-            return bayes_sol, bayes_score, bayes_eac
+    def search_firmware(self, algorithm="bayes"):
+        return self.algorithm_map[algorithm]()
 
-        elif algorithm == "linear":
-            e_time = time.time()
-            # Conduct a linear search
-            for k, v in self.param_op_map.items():
-                estimator = Estimator(architecture=self.architecture, operations=v)
-                estimation = estimator.estimate(["energy", "area", "cycle"], False)
-                energy, area, cycle = estimation
-                self.param_cost_map[k] = (score_firmware(energy, area, cycle), estimation)
-            self.top_solutions = sorted(((*v, k) for k, v in self.param_cost_map.items()), reverse=True)
-            print(self.top_solutions)
-            linear_score = abs(self.top_solutions[0][0])
-            linear_eac, linear_sol = self.top_solutions[0][1], self.top_solutions[0][2]
-            print(len(self.param_cost_map), "combinations estimated")
-            print("Exhaustive Search Time: ", time.time() - e_time)
-            return linear_sol, linear_score, linear_eac
-
-    def __bayesian_trial(self, **kwargs):
-        param_dict = OrderedDict(locals()['kwargs'])
-        # Make into discrete params
-        discrete_params = self.__make_discrete_param(param_dict)
-        # Get the operations for this discrete param
-        architecture, operations = self.architecture, self.param_op_map[discrete_params]
-        estimator = Estimator(architecture, operations)
-        energy, area, cycle = estimator.estimate(["energy", "area", "cycle"], analysis=False)
-        return score_firmware(energy, area, cycle)
-
-    def __make_discrete_param(self, continuous_param_set: OrderedDict):
+    def __bayesian_optimization_search(self):
         """
-        Round a continuous parameter set suggested by the Bayesian Model into a discrete parameter set that
-        is valid. Uses Euclidean distance algorithm
-        :param continuous_param_set: The set of continuous params, size N
-        :return: The parameter set made discrete, as an OrderedDict(). This will be put into **kwargs of Black Box Func
+        Employ Bayesian Optimization algorithm to search for optimal firmware solutions
+        :return: tuple of len == 3: (solution_arguments, score, (energy, area, cycle))
         """
-        continuous_param_ordered = [continuous_param_set[i] for i in self.fw_param_labels]
-        continuous_param = np.array(tuple(continuous_param_ordered))
-        euclid_distance = lambda x, y: np.sqrt(((x - y)**2).sum(axis=0))
-        distances = sorted([[euclid_distance(np.array(p), continuous_param), p] for p in self.param_op_map])
-        return distances[0][1]
+        def __bayesian_trial(**kwargs):
+            param_dict = OrderedDict(locals()['kwargs'])
+            # Make into discrete params
+            discrete_params = __make_discrete_param(param_dict)
+            # Get the operations for this discrete param
+            architecture, operations = self.architecture, self.param_op_map[discrete_params]
+            estimator = Estimator(architecture, operations)
+            energy, area, cycle = estimator.estimate(["energy", "area", "cycle"], analysis=False)
+            return score_firmware(energy, area, cycle)
+
+        def __make_discrete_param(continuous_param_set: OrderedDict):
+            """
+            Round a continuous parameter set suggested by the Bayesian Model into a discrete parameter set that
+            is valid. Uses Euclidean distance algorithm
+            :param continuous_param_set: The set of continuous params, size N
+            :return: The parameter set made discrete, as an OrderedDict(). This will be put into **kwargs of Black Box Func
+            """
+            continuous_param_ordered = [continuous_param_set[i] for i in self.fw_param_labels]
+            continuous_param = np.array(tuple(continuous_param_ordered))
+            euclid_distance = lambda x, y: np.sqrt(((x - y) ** 2).sum(axis=0))
+            distances = sorted([[euclid_distance(np.array(p), continuous_param), p] for p in self.param_op_map])
+            return distances[0][1]
+
+        b_start = time.time()
+        # Conduct Bayesian optimization over the firmware possibilities
+        # Set the parameter boundaries
+        param_bounds = OrderedDict()
+        fw_param_point_set = self.param_op_map.keys()
+        for i in range(len(self.fw_param_labels)):
+            dimension_i = [p[i] for p in fw_param_point_set]
+            # Heuristic: generally large tiles are more efficient
+            max_i, min_i = max(dimension_i) * 1.25, min(dimension_i) * 0.9
+            param_bounds[self.fw_param_labels[i]] = (min_i, max_i)
+        # Now apply the Bayesian model
+        seed_num = math.ceil(len(self.param_op_map) * 0.01)
+        bayes_model = BayesianOptimization(f=__bayesian_trial,
+                                                pbounds=param_bounds,
+                                                random_state=10,
+                                                verbose=True)
+        bayes_model.maximize(seed_num * 3, seed_num, kappa=1)
+        bayes_score = abs(bayes_model.max['target'])
+        bayes_p = __make_discrete_param(bayes_model.max['params'])
+        bayes_sol = {self.fw_param_labels[i]: bayes_p[i] for i in range(len(bayes_p))}
+        e = Estimator(self.architecture, self.param_op_map[bayes_p])
+        bayes_eac = e.estimate(['energy', 'area', 'cycle'], analysis=False)
+        # print("Bayes Firmware Estimate:", bayes_sol, "Score of:", bayes_score)
+        # print("Bayesian Time:", time.time() - b_start)
+        return bayes_sol, bayes_score, bayes_eac
+
+    def __linear_search(self):
+        e_time = time.time()
+        # Conduct a linear search
+        for k, v in self.param_op_map.items():
+            estimator = Estimator(architecture=self.architecture, operations=v)
+            estimation = estimator.estimate(["energy", "area", "cycle"], False)
+            energy, area, cycle = estimation
+            self.param_cost_map[k] = (score_firmware(energy, area, cycle), estimation)
+        self.top_solutions = sorted(((*v, k) for k, v in self.param_cost_map.items()), reverse=True)
+        print(self.top_solutions)
+        linear_score = abs(self.top_solutions[0][0])
+        linear_eac, linear_sol = self.top_solutions[0][1], self.top_solutions[0][2]
+        print(len(self.param_cost_map), "combinations estimated")
+        print("Exhaustive Linear Search Time: ", time.time() - e_time)
+        return linear_sol, linear_score, linear_eac
 
     def graph_energy_cycle(self):
         energy_data = tuple(math.log10(v[1][0]) for v in self.param_cost_map.values())
@@ -147,6 +161,7 @@ class Smapper:
         return self.param_op_map[param]
 
     def print_rankings(self, num=10):
+        # TODO: Legacy code from linear search
         print('{:^25} | {:^35} | {:^20}'.format('Score', 'Metrics', 'Inputs'))
         for i in range(num):
             if i >= len(self.top_solutions):